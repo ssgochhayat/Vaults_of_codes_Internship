7b573e5af57b2b31c904514a84d2f46cc8fe16d2
Suman Kumar Gochhayat <ssgochhayat2018@gmail.com>
2025-08-05 21:23:23 +0530

    first commit

---
+++ Assignment-1/Code4.py
@@ -0,0 +1,3 @@
+my_list = [1, 2, 3, 4, 5]
+print("The fifth element is: " + str(my_list[4]))
+#or print("The fifth element is: " + str(my_list[(len(my_list)-1)]))
---
+++ Assignment-1/Code7.py
@@ -0,0 +1,4 @@
+def multiply(a, b):
+    result = a * b
+    return result
+print(multiply(4, 5))
